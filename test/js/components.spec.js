9a0d3c80c22b5eb809b88c60da219f30440306fd
Thomas Scholtes <thomas-scholtes@gmx.de>
2016-09-11 16:53:30 +0200

    Simplify component specs

--- test/js/components.spec.js
+++ test/js/components.spec.js
@@ -2,24 +2,35 @@ import sinon from 'sinon'
 import {expect} from 'chai'
 
 import * as K from 'kefir'
-import {diff, patch, create, h} from 'virtual-dom'
+import {diff, patch, h} from 'virtual-dom'
+import * as VDOM from 'virtual-dom'
 import {jsdom} from 'jsdom'
-import proxyquire from 'proxyquire'
 
-let component = proxyquire('../../src/component', {
-  'raf': (fn) => component.raf(fn)
-}).default
-
+import component_ from '../../src/component'
 
 describe('component()', () => {
 
-  // TODO
-  function createDocument () {
-    return jsdom()
+  function createContext () {
+    let doc = jsdom()
+    let raf = sinon.stub()
+    return {
+      raf: raf,
+      doc: doc,
+      create (vnode) {
+        return VDOM.create(vnode, {document: doc})
+      },
+      component (nodes) {
+        return component_(nodes, {
+          document: doc,
+          raf: raf,
+        })
+      }
+    }
   }
 
+
   beforeEach(function () {
-    component.raf = sinon.stub()
+    Object.assign(this, createContext())
   })
 
   it('subscribes to stream when created', function () {
@@ -28,7 +39,7 @@ describe('component()', () => {
       .toProperty(() => h('div'))
 
     sinon.assert.notCalled(subscribe)
-    create(component(nodes))
+    this.create(this.component(nodes))
     sinon.assert.calledOnce(subscribe)
   })
 
@@ -36,10 +47,10 @@ describe('component()', () => {
     let subscribe = sinon.stub()
     let nodes = K.stream(subscribe)
       .toProperty(() => h('div'))
-    let comp = component(nodes)
+    let comp = this.component(nodes)
 
     let prev = h('div')
-    let el = create(prev)
+    let el = this.create(prev)
 
     sinon.assert.notCalled(subscribe)
     patch(el, diff(prev, comp))
@@ -50,8 +61,8 @@ describe('component()', () => {
     let unsubscribe = sinon.stub()
     let nodes = K.stream(() => unsubscribe)
       .toProperty(() => h('div'))
-    let comp = component(nodes)
-    let el = create(comp)
+    let comp = this.component(nodes)
+    let el = this.create(comp)
 
     sinon.assert.notCalled(unsubscribe)
     patch(el, diff(comp, h('div')))
@@ -61,29 +72,25 @@ describe('component()', () => {
   it('creates initial node immediately', function () {
     let getInitial = sinon.stub().returns(h('#initial'))
     let nodes = K.never().toProperty(getInitial)
-    let comp = component(nodes)
+    let comp = this.component(nodes)
 
-    let doc = createDocument()
     sinon.assert.notCalled(getInitial)
-    let el = create(comp, {document: doc})
+    let el = this.create(comp)
     sinon.assert.calledOnce(getInitial)
     expect(el.id).to.equal('initial')
   })
 
   it('updates node in dom after raf', function () {
-    let doc = createDocument()
-    let renderOpts = {document: doc}
-
     let emitter
     let nodes = K.stream((e) => { emitter = e })
       .toProperty(() => h('div'))
-    let comp = component(nodes, renderOpts)
-    let el = create(comp, renderOpts)
-    doc.body.appendChild(el)
+    let comp = this.component(nodes)
+    let el = this.create(comp)
+    this.doc.body.appendChild(el)
 
     emitter.emit(h('div#target'))
-    expect(doc.getElementById('target')).to.be.null
-    component.raf.yield()
-    expect(doc.getElementById('target')).not.to.be.null
+    expect(this.doc.getElementById('target')).to.be.null
+    this.raf.yield()
+    expect(this.doc.getElementById('target')).not.to.be.null
   })
 })
